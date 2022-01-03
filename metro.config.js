f66e62916ff453a1208c61fb0b604bf509edefad
Nico Neubig <nico@neubig.name>
2022-01-03 17:18:26 +0100

    build

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,4 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
 const defaultConfig = getDefaultConfig(__dirname);
-
-module.exports = {
-  resolver: {
-    assetExts: [...defaultConfig.resolver.assetExts, 'db'],
-  },
-};
+defaultConfig.resolver.assetExts.push('db');
+module.exports = defaultConfig;
